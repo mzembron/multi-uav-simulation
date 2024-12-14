06b27fde0eeac7bb6d7b568bd47066dca94c905f
mzembron <mateuszzembron@gmail.com>
2024-12-14 22:52:19 +0100

    feat: init navigation node from px4-ros2-interface-lib + change build system to CMake

--- navigation/src/main.cpp
+++ navigation/src/main.cpp
@@ -1,1 +1,15 @@
 // TODO: use here the navigation from px4-ros2-interface-lib/examples/cpp/modes/goto
+
+#include "rclcpp/rclcpp.hpp"
+
+#include <global_navigation.hpp>
+
+#include <local_navigation.hpp>
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ExampleLocalNavigationNode>());
+  rclcpp::shutdown();
+  return 0;
+}
